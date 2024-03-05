5ff98b6e50c12aaab563ac5f24e59c6a3ab7b323
Ibrahim Magdy <iibbrraahhimm00@gmail.com>
2024-03-05 21:54:11 +0200

    login_screen

--- lib/core/theme/application_theme.dart
+++ lib/core/theme/application_theme.dart
@@ -10,7 +10,10 @@ class ApplicationTheme {
       secondary: Colors.white,
     ),
     textTheme: TextTheme(
-        titleLarge: GoogleFonts.pacifico(color: Colors.white, fontSize: 78),
+        titleLarge: GoogleFonts.pacifico(
+          color: Colors.white,
+          fontSize: 78,
+        ),
         titleMedium: GoogleFonts.poppins(
           color: Colors.white,
           fontSize: 20,
@@ -20,6 +23,21 @@ class ApplicationTheme {
           color: const Color(0xff7DD7DF),
           fontSize: 16,
           fontWeight: FontWeight.w500,
+        ),
+        bodySmall: GoogleFonts.poppins(
+            fontSize: 15,
+            color: const Color(0xffACACAC),
+            fontWeight: FontWeight.w500),
+        displayMedium: GoogleFonts.poppins(
+          color: Colors.white,
+          fontSize: 35,
+          fontWeight: FontWeight.w500,
+          shadows: <Shadow>[
+            const Shadow(
+              offset: Offset(0, 5),
+              color: Color.fromARGB(255, 66, 61, 61),
+            ),
+          ],
         )),
   );
 }
